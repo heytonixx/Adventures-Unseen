29d156c761273b1673633c1c6a82d8338ad916e9
heytonixx <dongkiecanete0@gmail.com>
2024-09-24 06:41:12 +0800

    added assets and pages

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+
+function handleLogin(event) {
+    event.preventDefault(); // Prevent form submission
+    window.location.href = "../../pages/home.html"; // Redirect to home page
+}
+
+function handleSignUp(event) {
+    event.preventDefault(); // Prevent form submission
+    window.location.href = "../../signup.html"; // Redirect to signup page
+}
---
+++ assets/js/signup.js
@@ -0,0 +1,10 @@
+
+function handleLogin(event) {
+    event.preventDefault(); // Prevent form submission
+    window.location.href = "../../login.html"; // Redirect to home page
+}
+
+function handleSignUp(event) {
+    event.preventDefault(); // Prevent form submission
+    window.location.href = "../../pages/home.html"; // Redirect to signup page
+}
